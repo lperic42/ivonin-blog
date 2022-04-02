1593f6e355d57cfbbec6daff5eb6c7dd0bfd8e74
Luka <peric.luka@nth.ch>
2022-04-02 13:36:47 +0200

    added favicon

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Ivonin blog</title>
+    <link rel="icon" href="{{ url('img/favicon.png') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <script defer src="{{ mix('js/app.js') }}"></script>
     <script src="https://kit.fontawesome.com/6b159970d7.js" crossorigin="anonymous"></script>
